5b84c9b404bbffd0eb3c21495969cc27cafce223
thejc <threearapig@gmail.com>
2023-01-13 03:01:39 +0800

    the first stage

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 25;                    /* -c option; centers dmenu on screen */
+static int min_width = 1000;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Source Code Pro:size=16"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -19,8 +19,8 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOutHighlight] = { "#ffc978", "#00ffff" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
-static unsigned int lines      = 0;
-static unsigned int columns    = 0;
+static unsigned int lines      = 4;
+static unsigned int columns    = 3;
 
 /*
  * Characters not considered part of a word while deleting words
